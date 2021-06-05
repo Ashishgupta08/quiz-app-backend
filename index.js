cd31ca97a6aa1f0d43ae0c3393fa8ea08927f9d5
Ashish Gupta <59931129+Ashishgupta08@users.noreply.github.com>
2021-06-05 12:00:04 +0530

    .env added

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const mongoose = require('mongoose');
 
 app.use(cors());
 app.use(express.json());
 
+// mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const check = process.env.DB_HOST
+
 app.get('/', (req, res) => {
-    res.send('Hello Express app!')
+    res.send(check)
 });
 
 app.get('/user', (req, res) => {
