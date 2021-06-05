d05eb1b4949945024ce7bc05b12780cbb3397cdc
Ashish Gupta <59931129+Ashishgupta08@users.noreply.github.com>
2021-06-05 11:25:39 +0530

    files/folder structure created

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+
 app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello Express app!')
 });
 
 app.get('/user', (req, res) => {
-    res.send({username: "admin", password: "admin"})
+    res.json({username: "admin", password: "admin"})
 });
 
 app.listen(3000, () => {
     console.log('server started');
-});
+}); 
